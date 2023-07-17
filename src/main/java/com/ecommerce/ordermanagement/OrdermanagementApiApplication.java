9247dc2ba75f67739e6824dadce72cfc70d832cf
Nandan <Nandan1981@gmail.com>
2023-07-17 21:59:42 +0530

    added spring boot nature

---
+++ src/main/java/com/ecommerce/ordermanagement/OrdermanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermanagementApiApplication.class, args);
+	}
+
+}
